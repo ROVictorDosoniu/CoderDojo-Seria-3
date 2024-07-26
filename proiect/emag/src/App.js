4a38335510d16413bb450df7f3f4624c66cef13c
Ane0505 <147837593+Ane0505@users.noreply.github.com>
2024-07-26 15:36:54 +0300

    ajutor

--- proiect/emag/src/App.js
+++ proiect/emag/src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Poze from './pozeJos';
 
 function App() {
   return (  
-    <div className = "header">
-    <img src="emaglogo.png"/>
-    <input type="text" placeholder="începe o nouă căutare"></input>
+    <><div className="header">
+      <img src="emaglogo.png" />
+      <input type="text" placeholder="începe o nouă căutare"></input>
     </div>
+    
+    
+    
+    <div className="Jos">
+    <Poze/>
+      </div></>
   );
 }
 
