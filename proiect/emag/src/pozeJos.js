3da710f5d4ed5096127f052633e0045897ab7356
Ane0505 <147837593+Ane0505@users.noreply.github.com>
2024-07-26 15:06:39 +0300

    ajutor

---
+++ proiect/emag/src/pozeJos.js
@@ -0,0 +1,30 @@
+export default function Poze({}){
+    return(
+        <div>
+            <div className="pozaJos">
+                <p></p>
+                <img src="proiect/emag/public/pozeJos/animale.jpg"/>
+            </div>
+
+            <div className="pozaJos">
+                <p></p>
+                <img/>
+            </div>
+
+            <div className="pozaJos">
+                <p></p>
+                <img/>
+            </div>
+
+            <div className="pozaJos">
+                <p></p>
+                <img/>
+            </div>
+
+            <div className="pozaJos">
+                <p></p>
+                <img/>
+            </div>
+        </div>
+    )
+}
