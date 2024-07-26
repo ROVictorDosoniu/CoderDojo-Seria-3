8343506c83630f5c07dcf411ce9882d88844e536
Ane0505 <147837593+Ane0505@users.noreply.github.com>
2024-07-26 15:50:10 +0300

    fara probleme

--- proiect/emag/src/App.js
+++ proiect/emag/src/App.js
@@ -3,20 +3,18 @@ import './App.css';
 import Poze from './pozeJos';
 
 function App() {
-  return (  
+  return (
     <><div className="header">
       <img src="emaglogo.png" />
       <input type="text" placeholder="începe o nouă căutare"></input>
-    <div className = "header">
-    <img src="emaglogo.png"/>
-    <input type="text" placeholder="Începe o nouă căutare"></input>
+
     </div>
-    
-    
-    
-    <div className="Jos">
-    <Poze/>
-      </div></>
+
+
+      <div className="Jos">
+        <Poze />
+      </div>
+    </>
   );
 }
 
