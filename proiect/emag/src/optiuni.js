15e60ab817f09eb881bd1403275b6d965faf0347
ROVictorDosoniu <147833431+ROVictorDosoniu@users.noreply.github.com>
2024-07-26 15:08:56 +0300

    ok

--- proiect/emag/src/optiuni.js
+++ proiect/emag/src/optiuni.js
@@ -1,3 +1,49 @@
-export const chesti=[{
-    imagine:
-}]
+export const chesti=[
+    {
+    imagine:smartphone.png,
+    text:"Laptop, Tablete & Telefoane"
+},
+    {
+    imagine: ,
+    text:" PC, Periferice & Software"
+},
+{
+    imagine: ,
+    text:" TV, Audio-Video & Foto"
+},
+{
+    imagine: ,
+    text:" Electrocasnice & Climatizare"
+},{
+    imagine: ,
+    text:"Gaming, Carti & Birotica"
+},
+{
+    imagine: ,
+    text:" Bacanie"
+},
+{
+    imagine: ,
+    text:"Fashion"
+},
+{
+    imagine: ,
+    text:"Ingrijire personala & Cosmetice"
+},
+{
+    imagine: ,
+    text:"Casa, Gradina & Bricolaj"
+},
+{
+    imagine: ,
+    text:"Sport & Travel"
+},
+{
+    imagine: ,
+    text:"Auto, Moto & RCA"
+},
+{
+    imagine: ,
+    text:"Jucarii, Copii & Bebe"
+},
+]
